a38656112d38f8171675f8ea6addbb4289977939
Josh <fireworks.read@gmail.com>
2022-07-25 15:55:19 +0100

    Improved edges

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,63 +9,45 @@ public class PlayerMovement : MonoBehaviour
     public GameObject player;
     public Transform playerForwardWall;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        //Simple up down
+        if (transform.position.y < 4.5f && transform.position.y > -4.5f)
+        {
+            motion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            transform.Translate(motion * speed * Time.deltaTime);
+
+        }
+        else
+        {
+            if (player.transform.position.y > 4.5f)
+            {
+                transform.position = new Vector3(transform.position.x, 4.49f);
+            }
+            else if (player.transform.position.y < -4.5f)
+            {
+                transform.position = new Vector3(transform.position.x, -4.49f);
+            }
+
+        }
+
+        //if off to the left of the screen
+        if (player.transform.position.x < playerForwardWall.transform.position.x - 9f)
+        {
+            transform.position = new Vector3((playerForwardWall.transform.position.x - 9f) + 0.01f, transform.position.y);
+        }
+        //if got past the middle of the camera
+        else if (player.transform.position.x > playerForwardWall.transform.position.x)
+        {
+            transform.position = new Vector3(playerForwardWall.transform.position.x - 0.01f, transform.position.y);
+        }
     }
 
     //messwwwww with physics
     // move the player
     void FixedUpdate()
     {
-        //Simple up down
-        if(transform.position.y <= 4.5f && transform.position.y >= -4.5f)
-        {
-            motion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-            transform.Translate(motion * speed * Time.deltaTime);
-
-        }
-        else 
-        {
-            //Debug.Log("Player Y: " + player.transform.position.y);
-            if (player.transform.position.y > 4.5f)
-            {
- 
-                motion = new Vector2(motion.x,  -0.1f);
-                transform.Translate(motion * speed * Time.deltaTime);
-            }
-            else if (player.transform.position.y < -4.5f)
-            {
-                motion = new Vector2(motion.x, 0.1f);
-                transform.Translate(motion * speed * Time.deltaTime);
-            }
-            
-        }
-        
-        //if off to the left of the screen
-        if (player.transform.position.x < playerForwardWall.transform.position.x - 9f)
-        {
-            transform.position = new Vector3((playerForwardWall.transform.position.x - 9f) + 0.01f, transform.position.y);
-            motion = new Vector2(2f, motion.y);
-            transform.Translate(motion * speed * Time.deltaTime);
-        }
-        //if got past the middle of the camera
-        else if (player.transform.position.x > playerForwardWall.transform.position.x)
-        {
-            motion = new Vector2(-2f, motion.y);
-            transform.Translate(motion * speed * Time.deltaTime);
-        }
-
-
-
 
     }
 }
